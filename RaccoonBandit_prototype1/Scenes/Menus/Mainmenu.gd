b4d3ddcea388b44b0fc76aa0c9aac76a2394b5dc
isabel <128830650+isabelN-03@users.noreply.github.com>
2024-01-30 14:00:43 -0600

    idk main menu

--- RaccoonBandit_prototype1/Scenes/Menus/Mainmenu.gd
+++ RaccoonBandit_prototype1/Scenes/Menus/Mainmenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file( "res://Scenes/main_level.tscn")
+	get_tree().change_scene_to_file( "res://Scenes/mainScene.tscn")
 
 
 func _on_quit_pressed():
@@ -9,6 +9,5 @@ func _on_quit_pressed():
 
 
 func _on_tutorial_pressed():
-	get_tree().change_scene_to_file("res://tutorial page.tscn")
-
+	get_tree().change_scene_to_file("res://Scenes/Menus/tutorial page.tscn")
 
