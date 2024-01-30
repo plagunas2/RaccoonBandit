8fce91a3df78eb3b8f17e358cdb88bb75d6b141f
plagunas2 <priscilagunas@gmail.com>
2024-01-30 14:30:40 -0600

    creating separate obstacle generator script

--- RaccoonBandit_prototype1/Scenes/obstacles/ObstacleGenerator.gd
+++ RaccoonBandit_prototype1/Scenes/obstacles/ObstacleGenerator.gd
@@ -1,11 +1,34 @@
 extends CollisionShape2D
 
+var barrel_scene = preload("res://Scenes/obstacles/barrel.tscn")
+var cardboardBox_scene = preload("res://Scenes/obstacles/cardboardBox.tscn")
+var trashBag_scene = preload("res://Scenes/obstacles/trashBag.tscn")
+var mailbox_scene = preload("res://Scenes/obstacles/mailBox.tscn")
+var obstacle_types := [barrel_scene, cardboardBox_scene, trashBag_scene, mailbox_scene]
+var obstacles : Array
+
+var last_obs
+var generatorPos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	generatorPos = $obs_generator.position # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	generate_obs()
+	
+	
+	
+func generate_obs():
+	#generate obstacles
+	if obstacles.is_empty():
+		var obs_type = obstacle_types[randi() % obstacle_types.size()]
+		var obs
+		obs = obs_type.instantiate()
+		last_obs = obs
+		obs.position.x = generatorPos.x
+		obs.position.y = generatorPos.y
+		add_child(obs)
+		obstacles.append(obs)
--- RaccoonBandit_prototype1/Scenes/test_level.gd
+++ RaccoonBandit_prototype1/Scenes/test_level.gd
@@ -1,41 +1,15 @@
 extends Node2D
 
 
-#preload obstacles
-var barrel_scene = preload("res://Scenes/obstacles/barrel.tscn")
-var cardboardBox_scene = preload("res://Scenes/obstacles/cardboardBox.tscn")
-var trashBag_scene = preload("res://Assets/Obstacles/Garbage_Bag.png")
-var obstacle_types := [barrel_scene, cardboardBox_scene, trashBag_scene]
-var obstacles : Array
-
-#game variables
-#todo: player position, score, etc,
-var last_obs
-var generatorPos
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	 # Replace with function body.
-	generatorPos = $obs_generator.position
-	#new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
 	
-	generate_obs()
 	
 	
-	
-func generate_obs():
-	#generate obstacles
-	if obstacles.is_empty():
-		var obs_type = obstacle_types[randi() % obstacle_types.size()]
-		var obs
-		obs = obs_type.instantiate()
-		last_obs = obs
-		obs.position.x = generatorPos.x
-		obs.position.y = generatorPos.y
-		add_child(obs)
-		obstacles.append(obs)
