c4524c7c3f9e0f23d160c0525ec3a232a5bf2293
isabel <128830650+isabelN-03@users.noreply.github.com>
2024-02-04 17:17:10 -0600

    pause menu created

---
+++ RaccoonBandit_prototype1/Scenes/Menus/pause_Menu.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+func _on_quit_button_pressed():
+		get_tree().change_scene_to_file("res://Scenes/Menus/game_over_menu.tscn")
+
+func _on_retry_button_pressed():
+	pass # Replace with function body.
