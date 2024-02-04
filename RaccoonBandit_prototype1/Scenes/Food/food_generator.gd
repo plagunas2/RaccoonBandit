10c6f14d1b3d0a52a09773dbfa8e93ee1d8462d7
plagunas2 <priscilagunas@gmail.com>
2024-02-04 17:23:50 -0600

    Food generator (mostly) finished

--- RaccoonBandit_prototype1/Scenes/Food/food_generator.gd
+++ RaccoonBandit_prototype1/Scenes/Food/food_generator.gd
@@ -2,18 +2,26 @@ extends Node2D
 
 var food_arc_scene = preload("res://Scenes/Food/FoodArc.tscn")
 var food_types = [food_arc_scene]
-@onready var foodArcPos = $Marker2D #get pos for food arc, replace later
+
+var min_spawn_interval = 10.0 
+var max_spawn_interval = 15.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.start 
+	print("timer starts!")
+	$Timer.wait_time = randf_range(min_spawn_interval, max_spawn_interval)
+	$Timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _on_timer_timeout():
+	print("timer timeour!")
 	var food_type = food_types[randi() % food_types.size()].instantiate()
 	print(food_type) #debugging print statement
 	
-	food_type.position = foodArcPos.global_position
+	food_type.global_position = Vector2(0,0)
+	print("food pos ", food_type.global_position)
+	
+	add_child(food_type)
+	
+	$Timer.wait_time = randf_range(min_spawn_interval, max_spawn_interval)
+	
