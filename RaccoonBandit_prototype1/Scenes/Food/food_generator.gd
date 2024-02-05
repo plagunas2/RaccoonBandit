3e1d2925ade171ce24b9ea10e84cf915ead4ed86
plagunas2 <priscilagunas@gmail.com>
2024-02-04 18:46:05 -0600

    added more food items

--- RaccoonBandit_prototype1/Scenes/Food/food_generator.gd
+++ RaccoonBandit_prototype1/Scenes/Food/food_generator.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 var food_arc_scene = preload("res://Scenes/Food/FoodArc.tscn")
-var food_types = [food_arc_scene]
+var banana_scene = preload("res://Scenes/Food/Banana.tscn")
+var cheese_sandwich_scene = preload("res://Scenes/Food/CheeseSandwich.tscn")
+var flower_food_scene = preload("res://Scenes/Food/FoodMushFlowers.tscn")
+var food_types = [food_arc_scene, banana_scene, cheese_sandwich_scene, flower_food_scene]
 
 var min_spawn_interval = 10.0 
 var max_spawn_interval = 15.0
@@ -9,7 +12,7 @@ var max_spawn_interval = 15.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("timer starts!")
-	$Timer.wait_time = randf_range(min_spawn_interval, max_spawn_interval)
+	$Timer.wait_time = 3.0
 	$Timer.start()
 
 
