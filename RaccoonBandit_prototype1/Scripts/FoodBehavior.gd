30f94e1f331eba4835bce75cb589ec96a49150e1
plagunas2 <priscilagunas@gmail.com>
2024-02-05 18:10:19 -0600

    added food bite sound effect

--- RaccoonBandit_prototype1/Scripts/FoodBehavior.gd
+++ RaccoonBandit_prototype1/Scripts/FoodBehavior.gd
@@ -3,6 +3,7 @@ extends Area2D
 var speed 
 var parallax_background
 var scoreCounter
+var audio
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +11,12 @@ func _ready():
 		print(get_parent().name)
 		parallax_background = get_parent().get_parent().get_node("ParallaxBackground")
 		scoreCounter = get_parent().get_parent().get_node("HUD")
-	else: #if node is NOT in FoodGen node (aka, FoodArc node) -> the node is a level further 
-		#down so it needs to get another parent_node to access the parallax background
+		audio = get_parent().get_parent().get_node("AudioStreamPlayer2D")
+	else: #if node is NOT in FoodGen node (aka, in FoodArc node) -> the node is a level further 
+		#down so it needs to get another parent_node to access the parallax, score, & audio nodes
 		parallax_background = get_parent().get_parent().get_parent().get_node("ParallaxBackground")
 		scoreCounter = get_parent().get_parent().get_parent().get_node("HUD")
+		audio = get_parent().get_parent().get_parent().get_node("AudioStreamPlayer2D")
 	print(parallax_background)
 	speed = parallax_background.scroll_speed
 
@@ -25,6 +28,7 @@ func _process(delta):
 func _on_body_entered(body):
 	if (body.name == "Player"):
 		scoreCounter.increase_score()
+		audio.playEat()
 		queue_free()
 
 
