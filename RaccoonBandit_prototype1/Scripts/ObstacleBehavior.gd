8618bf7190f1c8a2a94a2c118882df009006ef64
LPedro20020 <pilopezvargas@knox.edu>
2024-02-18 03:05:52 -0600

    Deleting obstacles, food, floor tiles when they leave screen

--- RaccoonBandit_prototype1/Scripts/ObstacleBehavior.gd
+++ RaccoonBandit_prototype1/Scripts/ObstacleBehavior.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 var speed 
 var parallax_background
 
+var xpos
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	parallax_background = get_parent().get_parent().get_node("ParallaxBackground")
-	#speed = parallax_background.scroll_speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,3 +16,11 @@ func _process(delta):
 	else:
 		speed = 0
 	position += Vector2.LEFT * speed * delta
+	
+	xpos = self.global_position.x
+	left_screen()
+
+#to delete item when it leaves the screen
+func left_screen():
+	if (xpos < 0):
+		queue_free()
