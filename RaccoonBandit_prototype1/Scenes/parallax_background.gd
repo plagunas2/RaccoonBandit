3322d3220e9f31e42f31262b94d05990e3dcb8f0
LPedro20020 <pilopezvargas@knox.edu>
2024-02-18 04:13:46 -0600

    Background now speeds up

--- RaccoonBandit_prototype1/Scenes/parallax_background.gd
+++ RaccoonBandit_prototype1/Scenes/parallax_background.gd
@@ -2,6 +2,14 @@ extends ParallaxBackground
 
 var scroll_speed = 200
 
+func _ready():
+	$Timer.start()
+
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
 	scroll_base_offset -= Vector2(scroll_speed,0) * delta
+
+func _on_timer_timeout():
+	scroll_speed = scroll_speed + scroll_speed * 0.15
+	
+	$Timer.wait_time = 15
