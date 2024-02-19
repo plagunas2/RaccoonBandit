1187b894ca6cf334b67e3a6f64c3e88e58688ecc
rohaankhalid <rohaankhalid10@gmail.com>
2024-02-19 01:00:41 -0600

    connecting signal b/w HUD and npc scripts

--- RaccoonBandit_prototype1/Scenes/npc_player_connection.gd
+++ RaccoonBandit_prototype1/Scenes/npc_player_connection.gd
@@ -5,8 +5,10 @@ extends Node2D
 func _ready():
 	var npc = $npc
 	var player = $Player
+	var HUD = $HUD
 	
 	npc.connect("police_attack", Callable(player, "_on_police_attack"))
+	HUD.connect("score_6", Callable(npc, "_on_score_6"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
