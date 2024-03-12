4f5020ffc55dd6090c08a7f6a915efda70bb03a0
rohaankhalid <rohaankhalid10@gmail.com>
2024-03-12 11:26:35 -0500

    highscore added

--- RaccoonBandit_prototype1/Scenes/Menus/Game Over Menu.gd
+++ RaccoonBandit_prototype1/Scenes/Menus/Game Over Menu.gd
@@ -9,15 +9,27 @@ var current_score
 @onready var highscore_value: Label = $VBoxContainer5/HighscoreHBoxContainer/HighscoreValue
 
 func _ready():
+	var save_file = FileAccess.open("user://save.data", FileAccess.READ)
+	if save_file!=null:
+		highscore = save_file.get_32()
+		save_file.close()
+	else:
+		highscore = 0
+	
 	set_process_input(true)  # Enable input processing for this script
-	highscore = 0
-	current_score = 0
+	#highscore = 0
+	#current_score = 0
+	
+func save_game():
+	var save_file = FileAccess.open("user://save.data", FileAccess.WRITE)
+	if save_file:
+		save_file.store_32(highscore)
+		save_file.close()
 
 func _process(_delta):
 	# Check for mouse click in the _process function
 	#if Input.is_action_just_pressed("retry"):
 	#	_on_retry_pressed()
-
 	score_value.text = str(current_score)
 	highscore_value.text = str(highscore)
 
@@ -32,6 +44,7 @@ func _score_changed(new_score):
 	current_score = new_score
 	if current_score > highscore:
 		highscore = current_score
+		save_game()
 		highscore_value.text = str(highscore)
 		
 #func set_score(value):
